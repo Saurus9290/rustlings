d419936062c8ddba12d6e3d6ea1cecbd7316ca07
Saurabh <sauraj9290@gmail.com>
2024-09-02 15:58:34 +0100

    strings exercise 4 done

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,17 +1,28 @@
 fn trim_me(input: &str) -> &str {
-    // TODO: Remove whitespace from both ends of a string.
+    // Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There are multiple ways to do this.
+    // Add " world!" to the string.
+    format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons".
+    // Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
     // You can optionally experiment here.
+    let trimmed = trim_me("   Hello, Rust!   ");
+    println!("Trimmed: '{}'", trimmed);
+
+    let composed = compose_me("Hello");
+    println!("Composed: '{}'", composed);
+
+    let replaced = replace_me("I think cars are cool");
+    println!("Replaced: '{}'", replaced);
 }
 
 #[cfg(test)]
@@ -20,26 +31,4 @@ mod tests {
 
     #[test]
     fn trim_a_string() {
-        assert_eq!(trim_me("Hello!     "), "Hello!");
-        assert_eq!(trim_me("  What's up!"), "What's up!");
-        assert_eq!(trim_me("   Hola!  "), "Hola!");
-    }
-
-    #[test]
-    fn compose_a_string() {
-        assert_eq!(compose_me("Hello"), "Hello world!");
-        assert_eq!(compose_me("Goodbye"), "Goodbye world!");
-    }
-
-    #[test]
-    fn replace_a_string() {
         assert_eq!(
-            replace_me("I think cars are cool"),
-            "I think balloons are cool",
-        );
-        assert_eq!(
-            replace_me("I love to look at cars"),
-            "I love to look at balloons",
-        );
-    }
-}
