f662d69ffa1be361da966e17f44420459ac7e6cd
Saurabh <sauraj9290@gmail.com>
2024-09-07 14:58:45 +0100

    error exercse 3 done

--- exercises/13_error_handling/errors3.rs
+++ exercises/13_error_handling/errors3.rs
@@ -1,7 +1,3 @@
-// This is a program that is trying to use a completed version of the
-// `total_cost` function from the previous exercise. It's not working though!
-// Why not? What should we do to fix it?
-
 use std::num::ParseIntError;
 
 // Don't change this function.
@@ -13,14 +9,13 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     Ok(qty * cost_per_item + processing_fee)
 }
 
-// TODO: Fix the compiler error by changing the signature and body of the
-// `main` function.
-fn main() {
+// Fix the compiler error by changing the signature and body of the `main` function.
+fn main() -> Result<(), ParseIntError> {  // Changed signature to return Result
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    // Don't change this line.
-    let cost = total_cost(pretend_user_input)?;
+    // Handle the Result returned by total_cost.
+    let cost = total_cost(pretend_user_input)?;  // Using ? operator to propagate errors
 
     if cost > tokens {
         println!("You can't afford that many!");
@@ -28,4 +23,7 @@ fn main() {
         tokens -= cost;
         println!("You now have {tokens} tokens.");
     }
+
+    Ok(())  // Return Ok to satisfy the Result<(), ParseIntError> return type
 }
+
