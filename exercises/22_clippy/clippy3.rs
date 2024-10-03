bc0d727754d294c9e41bac4980da3517edca2821
Saurabh <sauraj9290@gmail.com>
2024-10-03 13:59:06 +0100

    clippy exercise 3 done

--- exercises/22_clippy/clippy3.rs
+++ exercises/22_clippy/clippy3.rs
@@ -1,27 +1,26 @@
-// Here are some more easy Clippy fixes so you can see its utility 📎
-// TODO: Fix all the Clippy lints.
-
-#[rustfmt::skip]
-#[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        println!("{:?}", my_option.unwrap());
+    // Using expect instead of unwrap with a helpful error message
+    if let Some(value) = my_option {
+        println!("{:?}", value);
+    } else {
+        println!("Option is None");
     }
 
+    // Fixing the array by adding commas between elements
     let my_arr = &[
-        -1, -2, -3
-        -4, -5, -6
+        -1, -2, -3,
+        -4, -5, -6,
     ];
-    println!("My array! Here it is: {my_arr:?}");
+    println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
-    println!("This Vec is empty, see? {my_empty_vec:?}");
+    // Fixing the empty vec resizing issue (remove resizing since it's unused)
+    let my_empty_vec: Vec<i32> = vec![];
+    println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
-    // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    // Using std::mem::swap to correctly swap the values
+    std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {value_a}; value b: {value_b}");
 }
